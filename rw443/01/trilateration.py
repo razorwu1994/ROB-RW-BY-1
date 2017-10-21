a503be37cce0a841c16292a2ca6375846acff1fa
razorwu1994 <razor.wu1994@gmail.com>
2017-10-21 13:38:20 -0400

    update

--- rw443/01/trilateration.py
+++ rw443/01/trilateration.py
@@ -172,16 +172,13 @@ def check_duplicate(points,testPoint):
     return completeDupFlag
 
 
-
-
-
 if __name__ == "__main__":
-    
+
     # Retrive file name for input data
-    if(len(sys.argv) == 1):
-        print ("Please enter data file name.")
+    if (len(sys.argv) == 1):
+        print "Please enter data file name."
         exit()
-    
+
     filename = sys.argv[1]
 
     # Read data
@@ -191,11 +188,11 @@ if __name__ == "__main__":
         distances.append(map(float, lines[line].split(' ')))
 
     # Print out the data
-    print ("The input four points and distances, in the format of [x, y, z, d], are:")
+    print "The input four points and distances, in the format of [x, y, z, d], are:"
     for p in range(0, len(distances)):
-        print (distances[p])
+        print distances[p]
 
-    # Call the function and compute the location 
+        # Call the function and compute the location
     location = trilaterate3D(distances)
-
-    print ("The location of the point is: " + str(location))
+    print
+    print "The location of the point is: " + str(location)
